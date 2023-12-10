e4585e0a2375dd5ae035c36035be208738ce7e2a
M.Batuhan TUNA <m.batuhantuna@MBatuhans-Air.lan>
2023-12-09 21:29:58 -0600

    adding my name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cubs Stink!!!
+         Mustafa Batuhan TUNA 
         </p>
         <a
           className="App-link"
